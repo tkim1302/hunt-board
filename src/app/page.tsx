86afeba0eab38488f81413de533c7a1ab20138f1
김태영 <zhtyzh0213@gmail.com>
2024-11-18 15:18:05 +0900

    feat: redirect to login page if user is not logged in on /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return <div></div>;
-}
+};
+
+export default Home;
