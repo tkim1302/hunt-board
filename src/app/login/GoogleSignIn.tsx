9d2fceb42b0977192fae3e671257a6cdf2ef41f0
김태영 <zhtyzh0213@gmail.com>
2024-11-18 14:34:10 +0900

    feat: implement google authentication functionality

--- src/app/login/GoogleSignIn.tsx
+++ src/app/login/GoogleSignIn.tsx
@@ -1,8 +1,16 @@
+"use client";
+
+import { signIn } from "next-auth/react";
 import Image from "next/image";
 
 const GoogleSignIn: React.FC = () => {
   return (
-    <div className="flex cursor-pointer gap-6 rounded-2xl border border-black p-6">
+    <div
+      onClick={() => {
+        signIn();
+      }}
+      className="flex cursor-pointer gap-6 rounded-2xl border border-black p-6"
+    >
       <Image src="/googleLogo.svg" width={40} height={40} alt="logo-google" />
       <h4 className="text-3xl">Sign In With Google</h4>
     </div>
