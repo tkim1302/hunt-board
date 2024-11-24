aa5f88f5ce79517ff8a094ae4bd0885871099586
김태영 <zhtyzh0213@gmail.com>
2024-11-25 02:56:41 +0900

    feat: add animation and update layout on login page

--- src/app/login/SignIn.tsx
+++ src/app/login/SignIn.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import Image from "next/image";
 
 const SignIn: React.FC = () => {
   return (
@@ -11,8 +10,7 @@ const SignIn: React.FC = () => {
       }}
       className="flex cursor-pointer gap-6 rounded-2xl border border-black p-6"
     >
-      <Image src="/googleLogo.svg" width={40} height={40} alt="logo-google" />
-      <h4 className="text-3xl">Sign In With Google</h4>
+      <h4 className="text-4xl font-semibold">LET'S GET STARTED!</h4>
     </div>
   );
 };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
-import SignIn from "./SignIn";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { redirect } from "next/navigation";
+import Layout from "./Layout";
 
 const page: React.FC = async () => {
   const session = await getServerSession(authOptions);
@@ -12,16 +12,7 @@ const page: React.FC = async () => {
 
   return (
     <div className="flex h-screen w-full justify-around">
-      <div className="flex basis-[65%] flex-col items-center justify-center gap-24 border-r border-gray-600">
-        <h1 className="font-dela text-8xl text-blue-500">HuntBoard</h1>
-        <h2 className="w-3/5 text-center text-5xl">
-          A POWERFUL TOOL TO TRACK YOUR JOB APPLICATIONS
-        </h2>
-      </div>
-      <div className="flex basis-[35%] flex-col items-center justify-center gap-16">
-        <h4 className="text-4xl font-semibold">LET&apos;S GET STARTED!</h4>
-        <SignIn />
-      </div>
+      <Layout />
     </div>
   );
 };
