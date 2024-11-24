00a04a648ae6db9daa392ed40fb8b388d79ab895
김태영 <zhtyzh0213@gmail.com>
2024-11-25 04:17:44 +0900

    feat: add animation on Loading component

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -1,5 +1,25 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
 const Loading: React.FC = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="flex h-full items-center justify-center">
+      <motion.div
+        animate={{
+          rotate: 360,
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 1,
+          ease: "linear",
+        }}
+      >
+        <Image src="/loading.png" width={60} height={60} alt="loading.png" />
+      </motion.div>
+    </div>
+  );
 };
 
 export default Loading;
