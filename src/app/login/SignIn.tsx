6a91fccf3e14301072a211229e2c41bb0a38608e
김태영 <zhtyzh0213@gmail.com>
2024-11-25 20:45:14 +0900

    fix: escape an unescaped apostrophe

--- src/app/login/SignIn.tsx
+++ src/app/login/SignIn.tsx
@@ -11,7 +11,7 @@ const SignIn: React.FC = () => {
       className="flex cursor-pointer gap-6 rounded-2xl border-2 border-black p-6 dark:border-gray-200"
     >
       <h4 className="text-4xl font-semibold dark:text-gray-200">
-        LET'S GET STARTED!
+        LET&apos;S GET STARTED!
       </h4>
     </div>
   );
